96d2fbb8bd1a21d1bf4d245ee6e002a8c8753996
TharinduKarunanayaka <76957102+TharinduKarunanayaka@users.noreply.github.com>
2021-06-15 21:22:32 +0530

    aaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselModule, WavesModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModulesPro, MDBSpinningPreloader } from 'ng-uikit-pro-standard';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MDBBootstrapModule,
+    AppRoutingModule,
+    CarouselModule,
+    WavesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
